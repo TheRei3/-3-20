9a9e1b0bc87ac24e0ad531ab93cc7784db5f20e7
admin <admin@DESKTOP-8L2IG92>
2021-04-27 06:29:41 +0900

    ex3no20

---
+++ ex3no20/src/ex3no20/ex3no20.java
@@ -0,0 +1,10 @@
+package ex3no20;
+
+public class ex3no20 { //108p
+	public static void main(String[] args) {
+		System.out.println(-10 % 8);
+		System.out.println(10 % -8);
+		System.out.println(-10 % -8);
+	}
+
+}
